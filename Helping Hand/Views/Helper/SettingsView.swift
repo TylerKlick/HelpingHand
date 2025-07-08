f399ac8c748e3aa2d289c500c6fa7fdbcc485521
Tyler Klick <tylerklick25@gmail.com>
2025-07-08 15:36:03 -0400

    [UI] Updated BluetoothDisabledView to be more readable and engaging

---
+++ Helping Hand/Views/Helper/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Helping Hand
+//
+//  Created by Tyler Klick on 7/8/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
