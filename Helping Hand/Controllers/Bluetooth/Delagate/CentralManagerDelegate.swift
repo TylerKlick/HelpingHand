d0a4ef3d00db22ef12a56c3bf3295049219338e0
Tyler Klick <tylerklick25@gmail.com>
2025-07-09 14:49:21 -0400

    [BLE] Updated BLE and interface for development

---
+++ Helping Hand/Controllers/Bluetooth/Delagate/CentralManagerDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  CentralManagerDelegate.swift
+//  Helping Hand
+//
+//  Created by Tyler Klick on 7/9/25.
+//
+
+import Foundation
---
+++ Helping Hand/Controllers/Bluetooth/Delagate/PeripheralDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  PeripheralDelegate.swift
+//  Helping Hand
+//
+//  Created by Tyler Klick on 7/9/25.
+//
+
+import Foundation
