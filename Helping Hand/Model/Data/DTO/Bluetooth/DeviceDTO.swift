f1899de2a6abc7f03555bcc5e93493bbf1490ce6
Tyler Klick <tjklick@ncsu.edu>
2025-08-03 17:30:28 -0400

    [In-progress] Adapting Async SwiftData with DTO

---
+++ Helping Hand/Model/Data/DTO/Bluetooth/DeviceDTO.swift
@@ -0,0 +1,24 @@
+////
+////  DeviceDTO.swift
+////  Helping Hand
+////
+////  Created by Tyler Klick on 8/2/25.
+////
+//
+//import Foundation
+//
+//struct DeviceDTO: Sendable {
+//    let id: UUID
+//    let identifier: UUID
+//    let name: String
+//    let dateAdded: Date
+//    let lastSeen: Date
+//    
+//    init(fromEntity device: Device) {
+//        self.id = device.id
+//        self.identifier = device.identifier
+//        self.name = device.name
+//        self.dateAdded = device.dateAdded
+//        self.lastSeen = device.lastSeen
+//    }
+//}
